e0034e23346ada5307c15f764c08febf95639a53
Sankalp Tiwari <41205888+SankalpSTiwari@users.noreply.github.com>
2024-01-16 01:22:18 -0800

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 // Components
 import Header from './components/Header';
-import FirstSection from './components/FirstSection';
+import Hero from './components/Hero';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
 import SecondSection from './components/SecondSection';
 import ThirdSection from './components/ThirdSection';
 import FourthSection from './components/FourthSection';
@@ -13,10 +15,12 @@ const App = () => {
   return (
     <div>
       <Header />
-      <FirstSection />
+      <Hero />
+      <Skills />
       <SecondSection />
-      <ThirdSection />
-      <FourthSection />
+      <Projects />
+      {/* <ThirdSection />
+      <FourthSection /> */}
       <Certifications />
       <Footer />
     </div>
